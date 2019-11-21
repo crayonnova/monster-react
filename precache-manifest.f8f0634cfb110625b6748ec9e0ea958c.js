82b7046737f24c4b2c901b6c33fff4f1d8f9950b
emo lay <julyemolay77@gmail.com>
2019-11-21 20:34:59 +0630

    Updates

---
+++ precache-manifest.f8f0634cfb110625b6748ec9e0ea958c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca24a93b170842a549bfda4e46f9b05",
+    "url": "/monster-react/index.html"
+  },
+  {
+    "revision": "8990420eb970e2c3748e",
+    "url": "/monster-react/static/css/main.296af5fd.chunk.css"
+  },
+  {
+    "revision": "a9dd641c9bc7d837f840",
+    "url": "/monster-react/static/js/2.47c22a6a.chunk.js"
+  },
+  {
+    "revision": "8990420eb970e2c3748e",
+    "url": "/monster-react/static/js/main.d2774f7b.chunk.js"
+  },
+  {
+    "revision": "45fe41b9b372f944f800",
+    "url": "/monster-react/static/js/runtime-main.10d093e4.js"
+  }
+]);
